45fbaabf282dcf2f5988b0f43f5e9531766f4754
PauliusTaraskevicius <fixas1029@gmail.com>
2023-01-12 00:21:10 +0200

    prisma schemas, api routes for user CRUD created

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,32 @@
-import { Typography } from '@mui/material';
+import { getAllUsers } from "../prisma/user";
 
-export default function Home() {
+export default function Home({ users }) {
   return (
     <>
-      <Typography variant='p'>Homepage</Typography>
+      {users.map((user) => (
+        <div key={user.id}>{user.name}</div>
+      ))}
     </>
   );
+}
+
+export async function getStaticProps() {
+  const users = await getAllUsers();
+
+  // Convert the updatedAt and createdAt in each user to string
+  // Otherwise, Next.js will throw an error
+  // Not required if you are not using the date fields
+
+  const updatedUsers = users.map((user) => ({
+    ...user,
+    updatedAt: user.updatedAt.toString(),
+    createdAt: user.createdAt.toString(),
+  }));
+
+  return {
+    props: {
+      users: updatedUsers,
+    },
+    revalidate: 60,
+  };
 }
---
+++ prisma/user.js
@@ -0,0 +1,51 @@
+import prisma from "./prisma";
+
+// READ
+export const getAllUsers = async () => {
+  const users = await prisma.user.findMany({});
+  return users;
+};
+
+export const getUser = async (id) => {
+  const user = await prisma.user.findUnique({
+    where: { id },
+  });
+  return user;
+};
+
+// CREATE
+export const createUser = async (email, name, lastName, phone, role) => {
+  const user = await prisma.user.create({
+    data: {
+      email,
+      name,
+      lastName,
+      phone,
+      role
+    },
+  });
+  return user;
+};
+
+// UPDATE
+export const updateUser = async (id, updateData) => {
+  const user = await prisma.user.update({
+    where: {
+      id,
+    },
+    data: {
+      ...updateData,
+    },
+  });
+  return user;
+};
+
+// DELETE
+export const deleteUser = async (id) => {
+  const user = await prisma.user.delete({
+    where: {
+      id,
+    },
+  });
+  return user;
+};
